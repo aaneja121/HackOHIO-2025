bc4dd27f85a27c17e76bd7530887d1bd3be1e1bc
aaneja121 <aaneja06@gmail.com>
2025-10-25 13:14:04 -0400

    backend

--- .next/types/app/page.ts
+++ .next/types/app/page.ts
@@ -1,4 +1,4 @@
-// File: C:\Users\hares\OneDrive\Desktop\CS Projects\HackOHIO\HackOHIO-2025\app\page.tsx
+// File: /Users/arihantaneja/Downloads/HackOHIO-2025/app/page.tsx
 import * as entry from '../../../app/page.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
